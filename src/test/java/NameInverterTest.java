d53f275e924c86aee9f911bc4d286cda28489fa2
kimtaesu <kimtaesoo188@gmail.com>
2017-09-06 21:02:38 +0900

    2. Assert First

--- src/test/java/NameInverterTest.java
+++ src/test/java/NameInverterTest.java
@@ -1,9 +1,13 @@
+import org.hamcrest.core.Is;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class NameInverterTest {
     @Test
     public void nameInverter()
     {
-
+        assertThat("", Is.is(""));
     }
 }
