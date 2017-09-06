edbee28dd11b72e2e46e3ff9a1fb14c9dfdf89f0
kimtaesu <kimtaesoo188@gmail.com>
2017-09-06 21:30:00 +0900

    10 . "Mr. first last" to "last, first" String[] -> List<String>

--- src/test/java/NameInverterTest.java
+++ src/test/java/NameInverterTest.java
@@ -1,6 +1,10 @@
 import org.hamcrest.core.Is;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertThat;
 
 public class NameInverterTest {
@@ -12,17 +16,20 @@ public class NameInverterTest {
         assertThat(invert("first last"), Is.is("last, first"));
         assertThat(invert("      name     "), Is.is("name"));
         assertThat(invert("first      last"), Is.is("last, first"));
+        assertThat(invert("Mr. first last"), Is.is("last, first"));
     }
 
     private String invert(String name) {
         if (name == null || name.isEmpty())
             return "";
         else {
-            String[] names = name.trim().split("\\s+");
-            if (names.length == 2)
-                return String.format("%s, %s", names[1], names[0]);
+            List<String> names = new ArrayList<>(Arrays.asList(name.trim().split("\\s+")));
+            if (names.size() > 1&& names.get(0).equals("Mr."))
+                names.remove(0);
+            if (names.size() == 2)
+                return String.format("%s, %s", names.get(1), names.get(0));
             else
-                return names[0];
+                return names.get(0);
         }
     }
 }
