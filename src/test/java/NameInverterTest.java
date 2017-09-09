f183047819cc777cea24830d6087b2302d55c792
kimtaesu <kimtaesoo188@gmail.com>
2017-09-09 21:23:30 +0900

    7. NameInvertor 생성     invert() 함수 내 Method Object     invert()를 제외하고 옮겨지지 않은 다른 함수들 NameInvertor로 이동

--- src/test/java/NameInverterTest.java
+++ src/test/java/NameInverterTest.java
@@ -25,42 +25,54 @@ public class NameInverterTest {
     }
 
     private String invert(String name) {
-        if (name == null || name.isEmpty())
-            return "";
-        else
-            return formatName(removeHonorific(splitNames(name)));
+        return new NameInvertor(name).invoke();
     }
 
-    private String formatName(List<String> names) {
-        if (names.size() == 1) {
-            return names.get(0);
+    private class NameInvertor {
+
+        private String name;
+        public NameInvertor(String name) {
+            this.name = name;
         }
-        return formatMultiElementName(names);
-    }
 
-    private String formatMultiElementName(List<String> names) {
-        String postNominal = "";
-        if (names.size() > 2)
-            postNominal = getPostNominal(names);
-        return String.format("%s, %s %s", names.get(1), names.get(0), postNominal).trim();
-    }
+        private String formatMultiElementName(List<String> names) {
+            String postNominal = "";
+            if (names.size() > 2)
+                postNominal = getPostNominal(names);
+            return String.format("%s, %s %s", names.get(1), names.get(0), postNominal).trim();
+        }
 
-    private String getPostNominal(List<String> names) {
-        List<String> postNominals = names.subList(2, names.size());
-        return Joiner.on(" ").join(postNominals);
-    }
+        private String getPostNominal(List<String> names) {
+            List<String> postNominals = names.subList(2, names.size());
+            return Joiner.on(" ").join(postNominals);
+        }
 
-    private List<String> removeHonorific(List<String> names) {
-        if (names.size() > 1 && isHonoritic(names))
-            names.remove(0);
-        return names;
-    }
+        private boolean isHonoritic(List<String> names) {
+            return names.get(0).matches("Mr\\.|Mrs\\.");
+        }
 
-    private List<String> splitNames(String name) {
-        return new ArrayList<>(Arrays.asList(name.trim().split("\\s+")));
-    }
+        public String invoke() {
+            if (name == null || name.isEmpty())
+                return "";
+            else
+                return formatName(removeHonorific(splitNames(name)));
+        }
 
-    private boolean isHonoritic(List<String> names) {
-        return names.get(0).matches("Mr\\.|Mrs\\.");
+        private String formatName(List<String> names) {
+            if (names.size() == 1) {
+                return names.get(0);
+            }
+            return formatMultiElementName(names);
+        }
+
+        private List<String> removeHonorific(List<String> names) {
+            if (names.size() > 1 && isHonoritic(names))
+                names.remove(0);
+            return names;
+        }
+
+        private List<String> splitNames(String name) {
+            return new ArrayList<>(Arrays.asList(name.trim().split("\\s+")));
+        }
     }
 }
